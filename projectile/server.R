0fa6a23e9848c6a6e5bb9262327e687d68fc6dad
weslwz <weslzhou@gmail.com>
2014-07-27 16:42:52 -0400

    server and ui

---
+++ projectile/server.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  output$trajplot <- renderPlot({
+    g <- 9.81
+    x0 <- 0
+    y0 <- input$height
+    s0 <- input$speed
+    a0 <- input$angle
+    vx0 <- s0*cos(a0/180*pi)
+    vy0 <- s0*sin(a0/180*pi)
+
+    # time until hit ground
+    t <- (sqrt(vy0^2 + 2*g*y0) + vy0) / g
+
+    # computes the trajectory
+    T <- seq(0, t, length.out=300)
+    X <- vy0 * T
+    Y <- -0.5*g*T^2 + vy0*T+y0
+
+    if(input$xmax >  input$xmin & input$ymax>input$ymin) {
+      plot(X, Y, type="l", xlab="distance (m)", ylab="height (m)", xlim=c(input$xmin,input$xmax), ylim=c(input$ymin,input$ymax))
+      text(x0, y0, "start", col="red")
+    }
+  })
+})
---
+++ projectile/ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Projectile Trajectory"),
+  sidebarLayout(
+    sidebarPanel(
+        wellPanel(
+          h4("Trajectory Control", style="color:blue"),
+          numericInput("height", label = "Initial Height (m)", value = 30, min=0, step=0.1),
+          numericInput("speed", label = "Initial Speed (m/s)", value = 15, min=0, step=0.1),
+          sliderInput("angle", label = "Angle (deg)", value = 80, min = 0, max = 360, step=0.1)
+        ),
+        wellPanel(
+          h4("Axis Control", style="color:blue"),
+          fluidRow(
+            column(4, numericInput("xmin", "x-min", value=0, min=0)),
+            column(4, numericInput("xmax", "x-max", value=100))
+          ),
+          fluidRow(
+            column(4, numericInput("ymin", "y-min", value=0)),
+            column(4, numericInput("ymax", "y-max", value=100))
+          )
+        )
+    ),
+
+
+    mainPanel(plotOutput("trajplot"))
+  )
+))
